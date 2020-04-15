cbe00c6ce12af6417c8f70c863abf28c0e113345
JackScanlon <jackascanlon@hotmail.co.uk>
2020-04-15 22:02:53 +0100

    Updates

---
+++ precache-manifest.f1dd22888097bba20c6423de45a347eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d21d65a7e39111896e5c776621262f",
+    "url": "/WhatsAppVisualisation/index.html"
+  },
+  {
+    "revision": "d2e4fb442fa8a5564acd",
+    "url": "/WhatsAppVisualisation/static/css/main.ec29383b.chunk.css"
+  },
+  {
+    "revision": "f66c4f3e4384149dcded",
+    "url": "/WhatsAppVisualisation/static/js/2.dad70e29.chunk.js"
+  },
+  {
+    "revision": "56d4ad2eeae512293c5f2334f8d92bc8",
+    "url": "/WhatsAppVisualisation/static/js/2.dad70e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e4fb442fa8a5564acd",
+    "url": "/WhatsAppVisualisation/static/js/main.0fe17a1f.chunk.js"
+  },
+  {
+    "revision": "0b75e25e9fc88c659e0a",
+    "url": "/WhatsAppVisualisation/static/js/runtime-main.d5511a16.js"
+  }
+]);
